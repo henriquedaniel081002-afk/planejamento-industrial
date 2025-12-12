99711207bd07ccf8f998ce2a84d97fe97ea54a0c
Elisangela Silva <elisangela.silva@rymocorp.local>
2025-12-12 16:35:40 -0400

    config firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,2 +1,14 @@
-// Arquivo desativado. O aplicativo utiliza LocalStorage.
-export {};
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDs0jTJZUnYQVHUILDxMAUJxqIgYEj6g8",
+  authDomain: "planejamento-industrial-8d5bf.firebaseapp.com",
+  projectId: "planejamento-industrial-8d5bf",
+  storageBucket: "planejamento-industrial-8d5bf.firebasestorage.app",
+  messagingSenderId: "889772228544",
+  appId: "1:889772228544:web:34b3d854f4160fbe47d225"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
